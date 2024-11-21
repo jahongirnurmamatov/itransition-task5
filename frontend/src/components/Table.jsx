4e7f796202ff78dd7f1c966e25f43703beb1cc94
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-11-22 00:57:34 +0500

    Table arrow optimization

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import { tableData } from "../../public/assets";
 import ExpandedRow from "./ExpandedRow";
 import useBookStore from "../store/bookStore";
-
+import { IoIosArrowDown } from "react-icons/io";
 const Table = () => {
   const [expandedRow, setExpandedRow] = useState(null);
-  const {averageLikes,averageReviews,seed,lang} = useBookStore();
-  console.log(averageLikes,averageReviews,seed,lang)
+  const { averageLikes, averageReviews, seed, lang } = useBookStore();
+
   return (
     <div className="overflow-x-auto px-20 mt-10 bg-white">
       <table className="table w-full border ">
@@ -24,7 +24,7 @@ const Table = () => {
         {/* Table Body */}
         <tbody>
           {tableData.map((book, index) => (
-            <React.Fragment key={book.id} >
+            <React.Fragment key={book.id}>
               {/* Collapsible Row */}
               <tr
                 className={`cursor-pointer font-semibold py-2 ${
@@ -38,32 +38,21 @@ const Table = () => {
                 <td>{book.isbn}</td>
                 <td>{book.title}</td>
                 <td>{book.author}</td>
-                <td>{book.publisher}</td>
-                <td>
-                  <svg
-                    className={`w-3 h-3 shrink-0 transform transition-transform duration-300 ${
+                <td className="flex justify-between">
+                  {book.publisher}{" "}
+                  <IoIosArrowDown
+                    className={`w-5 h-5 shrink-0 transform transition-transform duration-300 ${
                       expandedRow === index ? "rotate-180" : ""
                     }`}
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 10 6"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 5 5 1 1 5"
-                    />
-                  </svg>
+                  />
                 </td>
+                <td></td>
               </tr>
 
               {/* Accordion for Expanded Details */}
               {expandedRow === index && <ExpandedRow book={book} />}
             </React.Fragment>
-          ))}  
+          ))}
         </tbody>
       </table>
     </div>
