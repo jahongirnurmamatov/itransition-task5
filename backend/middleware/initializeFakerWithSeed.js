4bdd0293c64d2bf99ab951afb59408e1befde704
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-11-21 21:28:47 +0500

    generate randomReview function

--- backend/middleware/initializeFakerWithSeed.js
+++ backend/middleware/initializeFakerWithSeed.js
@@ -7,3 +7,11 @@ function initializeFakerWithSeed(seed) {
   faker.seed(rng());
 }
 
+// to generate random review from the seed rng given
+function generateRandomReview(rng) {
+    const rating = Math.floor(rng() * 5) + 1;
+    const text = faker.lorem.sentence();
+    const author = faker.name.findName();
+
+    return { rating, text, author };
+}
