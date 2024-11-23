3f087377b5c830dc660eae76418931744f726e85
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-11-23 13:59:13 +0500

    grid view responsiveness

--- frontend/src/components/GridView.jsx
+++ frontend/src/components/GridView.jsx
@@ -4,19 +4,31 @@ import useBookStore from "../store/bookStore";
 const GridView = () => {
   const { books } = useBookStore();
   return (
-    <div className="w-full px-20 py-10">
+    <div className="w-full lg:px-20 py-10 md:px-10 sm:px-5 px-0">
       <div className="grid xl:grid-cols-3 2xl:grid-cols-4 md:grid-cols-2 lg:gap-8 md:gap-4 gap-5 ">
         {books.map((book) => (
-          <div className="card  card-side bg-base-100 md:w-[350px]  shadow-xl">
+          <div className="card  card-side bg-base-100 md:w-[350px] w-full shadow-xl">
             <figure className="flex-1">
-              <img
-                src="./book-cover.webp"
-                className="object-contain w-full h-auto"
-                alt=""
-              />
+              <div className="relative">
+                <img
+                  src="./book-cover.webp"
+                  className="object-contain w-full h-auto"
+                  alt=""
+                />
+                <div className="absolute top-10 left-4 text-gray-800 text-center flex flex-col gap-2">
+                  <p className="">
+                    {book.title}
+                  </p>
+                  <p className=" italic text-gray-600">
+                    {book.auhtor}
+                  </p>
+                </div>
+              </div>
             </figure>
-            <div className="card-body flex-1 ">
-              <h2 className="card-title  font-bold font-serif">{book.title}</h2>
+            <div className="card-body flex-1 md:text-sm text-md bg-base-300">
+              <h2 className="card-title  font-bold font-serif  md:text-sm text-md">
+                {book.title}
+              </h2>
               <p className="italic font-thin">{book.author}</p>
               <p className="text-info">{book.publisher}</p>
             </div>
